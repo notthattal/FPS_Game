edffa2e2574c609295f9621ac75abb23522795da
Tal Erez <>
2020-05-24 00:08:28 -0700

    Created Sandbox scene with functioning enemy and a player able to shoot and effects along with the weapon. Started working on animator controller, but haven't added functionality yet

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitpoints = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        hitpoints = hitpoints - damage;
+        if (hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
