729a3fd4dea504eb08d44b79ba306bd857ff086f
Tal Erez <>
2020-05-30 18:57:52 -0700

    Created mechanics for the flashlight and a battery pickup

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 90f;
+    [SerializeField] float intensityAmount = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponentInChildren<Flashlight>().RestoreLightAngle(restoreAngle);
+            other.GetComponentInChildren<Flashlight>().RestoreLightIntensity(intensityAmount);
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight = default;
+
+    private void Start()
     {
-        
+        myLight = GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void RestoreLightIntensity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
     }
 }
